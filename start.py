df2f1cc2e8c0aa13a2359e2e5233832c1903e742
Matthew Kob <matt.kob@hotmail.com>
2019-08-19 13:37:47 -0400

    second commit

--- start.py
+++ start.py
@@ -5,21 +5,19 @@ from os import rename
 import requests
 
 
-name = input("Your name? ")
-print("Hello,", name)
-# import rename
+import rename
 
-# print(sys.version)
-# print(sys.executable)
+print(sys.version)
+print(sys.executable)
 
 
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
 
 
-# print(greet('World'))
-# print(greet('Corey'))
+print(greet("World"))
+print(greet("Corey"))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
